be48a627ea32850c4f53091f43a0695c313206d6
harshita795 <yadavharshita261303@gmail.com>
2024-12-10 13:22:40 +0530

    feat: music model

---
+++ BD_7.1_HW_2/models/music.models.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const musicSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      enum: [
+        "Rock",
+        "Pop",
+        "Hip-Hop",
+        "Jazz",
+        "Classical",
+        "Country",
+        "Electronic",
+        "R&B",
+        "Reggae",
+        "Indie",
+      ],
+    },
+    releaseYear: {
+      type: Number,
+    },
+    recordLabel: {
+      type: String,
+    },
+    format: {
+      type: String,
+    },
+    isExplicit: {
+      type: Boolean,
+      default: false,
+    },
+    isAvailableOnStreaming: {
+      type: Boolean,
+      default: false,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Music = mongoose.model("Music", musicSchema);
+
+module.exports = Music;
